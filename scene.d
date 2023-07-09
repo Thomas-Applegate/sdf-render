5f53d0f777276d9877f663a48b6a01383791fa91
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-07-08 21:13:47 -0600

    ppm test

--- scene.d
+++ scene.d
@@ -14,5 +14,16 @@ class Scene {
 
 void main(string[] args)
 {
+	//test the ppm save function
+	//image should be one row of pure r,g,b
+	//and one row of pure c,m,y
+	PPM i = new PPM(3, 2);
+	i.setPixel(0,0, vec3(1.0,0.0,0.0));
+	i.setPixel(1,0, vec3(0.0,1.0,0.0));
+	i.setPixel(2,0, vec3(0.0,0.0,1.0));
+	i.setPixel(0,1, vec3(0.0,1.0,1.0));
+	i.setPixel(1,1, vec3(1.0,0.0,1.0));
+	i.setPixel(2,1, vec3(1.0,1.0,0.0));
 	
+	i.save("image-test.ppm");
 }
